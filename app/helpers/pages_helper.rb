a0d69a54f2192bbe9b9a7fb1500a31617215c410
Kaidong Hu <dongforregister@gmail.com>
2024-04-24 12:59:57 -0400

    Refactor account widget and update styles for dropdown
    Refactor account widget to use a helper method for frame ID, streamline
    dropdown\
    styles in SCSS, and remove redundant partials for accounting widget.
    Also, suppress flash messages on full page reloads and adjust navigation
    links to use button\
    styles.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,6 @@
 module PagesHelper
+  def account_widget_frame_id = 'account-widget-frame'
   def account_widget_frame_tag(**attributes, &block)
-    turbo_frame_tag 'account-widget-frame', target: modal_frame_id, **attributes, &block
+    turbo_frame_tag account_widget_frame_id, target: modal_frame_id, **attributes, &block
   end
 end
